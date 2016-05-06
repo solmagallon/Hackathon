73195327cf79f5a3aad428886a6f7001b3e79b93
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-05-06 09:59:40 -0700

    add bower and components

---
+++ app/app.js
@@ -0,0 +1,3 @@
+(function(){
+	var app = anuglar.module('app', []);
+})();
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp'),
+   livereload = require('gulp-livereload'),// auto-reload browser when files are changed 
+   wiredep = require('wiredep').stream,
+   gutil = require('gulp-util'),
+   connect = require('gulp-connect'),      // run a local dev server
+   inject = require('gulp-inject'),        // inject app dependency includes on index.html
+   open = require('gulp-open');            // open a URL in the browser
+   var git = require('gulp-git');
+
+var jsSources = ['app/*.js'],
+   cssSources = ['app/**/*.css'],
+   htmlSources = ['**/*.html'];
+
+var paths = ['./bower_components/','./app/*.js','./app/**/*.css'];
+
+gulp.task('init', function(){
+  git.init(function(err){
+    if (err) throw err;
+  });
+});
+
+gulp.task('add', function(){
+  return gulp.src('*.html')
+  .pipe(git.add());
+});
+
+gulp.task('commit', function(){
+  return gulp.src('*.html')
+  .pipe(git.commit('initial commit.'));
+});
+
+gulp.task('initCommit', ['init', 'add', 'commit']);
+
+gulp.task('connect', function() {
+   connect.server({
+       root: '.',
+       livereload: true
+   })
+});
+
+gulp.task('app', function(){
+   var options = {
+       uri: 'http://localhost:8080',
+       app: 'chrome'
+   };
+   gulp.src('./index.html')
+       .pipe(open(options));
+});
+
+// Watch
+gulp.task('watch', function() {
+   gulp.watch(jsSources, ['js']);
+   gulp.watch(cssSources, ['css']);
+   gulp.watch(htmlSources, ['html']);
+});
+
+
+gulp.task('injectables', function() {
+   var sources = gulp.src(paths, {read: false});
+   return gulp.src('index.html')
+       .pipe(wiredep())
+       .pipe(inject(sources))
+       .pipe(gulp.dest('.'));
+});
+
+gulp.task('js', function() {
+   gulp.src(jsSources)
+       .pipe(connect.reload())
+});
+
+gulp.task('html', function() {
+   gulp.src(htmlSources)
+       .pipe(connect.reload())
+});
+
+gulp.task('css', function() {
+   gulp.src(cssSources)
+       .pipe(connect.reload())
+});
+
+gulp.task('serve', ['connect', 'watch', 'injectables', 'app', 'initCommit']);
