ffbccf2270586255ac4dbb604e5936276645a3e3
Michael Whitmore <mwhitmorelaw@gmail.com>
2016-05-09 12:49:44 -0700

    clean up code. begin adding location search for videos

--- app/queryFactory.js
+++ app/queryFactory.js
@@ -12,15 +12,30 @@
 
 		return service;
 
-		function getData(url){
+		function getData(trail){
 			const VIDEO_URL = "https://www.youtube.com/embed/";
+
+			// set up the url for the API query.
+			const YOUTUBE_KEY = "&key=AIzaSyAGGi10zMvYlSfc8m_VsOIuesj-D1i6HeM";
+			const YOUTUBE_URL = "https://www.googleapis.com/youtube/v3/search?&part=snippet&type=video&maxResults=8&order=relevance";
+
+			var query = YOUTUBE_URL;
+			query += YOUTUBE_KEY;
+			query += "&q=hiking+";
+			query += trail.name;
+			// if a latitude and longitude was provided with trail search based on location.
+			// if ((trail.lat!=0) && (trail.lon != 0)){
+			// 	query += "&location=" + trail.lat + "%252C+" + trail.lon;
+			// 	query += "&locationRadius=25.0mi"
+			// } 
+
 			var defer = $q.defer();
 			$http({
 				method:'GET',
-				url: url
+				url: query
 			}).then(function(response){
 				if (typeof response.data === 'object'){
-					toastr.success('Data retrieved successfully.');
+					toastr.success(trailName + ' data retrieved successfully.');
 					defer.resolve(response);
 					console.log(response.data)
 					angular.forEach(response.data.items, function(element){
